79b4fb77f158b1eacb74930dc81d8fecd8277189
chanok <Chanok_boon@hotmail.com>
2020-11-07 23:36:55 +0700

    Back1

--- app/src/main/java/com/example/login_register/login_new.java
+++ app/src/main/java/com/example/login_register/login_new.java
@@ -84,7 +84,7 @@ public class login_new extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(),register.class));
-                startActivity(new Intent(getApplicationContext(),register.class));
+               
 
             }
         });
