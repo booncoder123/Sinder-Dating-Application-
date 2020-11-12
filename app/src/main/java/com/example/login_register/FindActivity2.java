693a78dc9a622ec7ccd500d9d0cee26e673a9061
chanok <Chanok_boon@hotmail.com>
2020-11-13 02:44:31 +0700

    still!

--- app/src/main/java/com/example/login_register/FindActivity2.java
+++ app/src/main/java/com/example/login_register/FindActivity2.java
@@ -10,12 +10,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -32,6 +37,7 @@ public class FindActivity2 extends AppCompatActivity {
         UserRef = FirebaseDatabase.getInstance().getReference().child("MyUsers");
 
 
+
         FindFriendRecycleList = (RecyclerView) findViewById(R.id.find_friends_recycle_list);
         FindFriendRecycleList.setHasFixedSize(true);
         FindFriendRecycleList.setLayoutManager(new LinearLayoutManager(this));
@@ -43,16 +49,37 @@ public class FindActivity2 extends AppCompatActivity {
 
         super.onStart();
 
-        FirebaseRecyclerOptions<Contacts> options =
-                new FirebaseRecyclerOptions.Builder<Contacts>()
-                        .setQuery(UserRef,Contacts.class)
-                        .build();
-        FirebaseRecyclerAdapter<Contacts,FindFriendViewHolder> adapter =
-                new FirebaseRecyclerAdapter<Contacts, FindFriendViewHolder>(options) {
+
+
+        FirebaseRecyclerOptions<Contacts> options = new FirebaseRecyclerOptions.Builder<Contacts>().setQuery(UserRef,Contacts.class).build();
+        FirebaseRecyclerAdapter<Contacts,FindFriendViewHolder> adapter = new FirebaseRecyclerAdapter<Contacts, FindFriendViewHolder>(options) {
                     @Override
                     protected void onBindViewHolder(@NonNull FindFriendViewHolder holder, int position, @NonNull Contacts model) {
-                        holder.userName.setText(""+model.getName());
-                        holder.userStatus.setText(model.getStatus());
+                        getRef(position).addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                if(!snapshot.child("checked").exists()){
+                                    holder.userName.setText(""+model.getName());
+                                    holder.userStatus.setText(model.getStatus());
+                                }
+
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+//                        Toast.makeText(FindActivity2.this,x,Toast.LENGTH_SHORT).show();
+
+
+                        holder.add_button.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+//                                String eiei = getRef(position).child("sex").getKey();
+//                                Toast.makeText(FindActivity2.this,eiei,Toast.LENGTH_SHORT).show();
+                            }
+                        });
 
                     }
 
@@ -75,13 +102,15 @@ public class FindActivity2 extends AppCompatActivity {
 //    }
     public  static class FindFriendViewHolder extends RecyclerView.ViewHolder{
         TextView userName,userStatus;
+        Button add_button;
 //        CircleImageView profileImage;
 
         public FindFriendViewHolder(@NonNull View itemView) {
             super(itemView);
             userName = itemView.findViewById(R.id.textviewid);
             userStatus = itemView.findViewById(R.id.textviewname);
-//            profileImage = itemView.findViewById(R.id.users_profile_image);
+            add_button = itemView.findViewById(R.id.add_button);
+//
 
         }
     }
